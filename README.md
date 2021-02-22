457faf6d6547640bb9e6aac6ce57b88372aa9868
Frank <frank@weborama.nl>
2021-02-22 23:50:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+ A small portfolio project that might have some more random stuff added to it. To learn about webpack and react
---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import "./style.css";
+// import "./style.scss";
+// console.log("Hello webpack!");
+
+// const fancyFunc = () => {
+//   return [1, 2];
+// };
+
+// const [a, b] = fancyFunc();
+
+import React, { useState } from "react";
+import { render } from "react-dom";
+
+function App() {
+    const [state, setState] = useState("CLICK ME");
+
+    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+}
+
+render(<App />, document.getElementById("root"));
